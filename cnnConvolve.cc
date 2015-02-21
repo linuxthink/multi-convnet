a70da0d4721acde35447ebfe986a7625adb68225
Yixing Lao <linuxthink@gmail.com>
2015-02-21 12:55:35 -0800

    move out util

--- cnnConvolve.cc
+++ cnnConvolve.cc
@@ -5,65 +5,6 @@
 #include <string.h>
 #include <algorithm>
 
-bool convolve2D(double *in, int inDim, double *kernel, int kernelDim,
-                double *out) {
-// This function is from the following source:
-// AUTORIGHTS
-// -------------------------------------------------------
-// Copyright (C) 2011-2012 Ross Girshick
-// This file is part of the voc-releaseX code
-// (http://people.cs.uchicago.edu/~rbg/latent/)
-// and is available under the terms of an MIT-like license
-// provided in COPYING. Please retain this notice and
-// COPYING if you use this file (or a portion of it) in
-// your project.
-// -------------------------------------------------------
-  int outDim = inDim - kernelDim + 1;
-
-  double *A_src = in;
-  double *B_src = kernel;
-  double *dst = out;
-  // start convolution
-  for (int x = 0; x < outDim; x++) {
-    for (int y = 0; y < outDim; y++) {
-      double val = 0;
-      for (int xp = 0; xp < kernelDim; xp++) {
-        double *A_off = A_src + (x+xp)*inDim + y;
-        double *B_off = B_src + xp*kernelDim;
-        switch(kernelDim) {
-          case 20: val += A_off[19] * B_off[19];
-          case 19: val += A_off[18] * B_off[18];
-          case 18: val += A_off[17] * B_off[17];
-          case 17: val += A_off[16] * B_off[16];
-          case 16: val += A_off[15] * B_off[15];
-          case 15: val += A_off[14] * B_off[14];
-          case 14: val += A_off[13] * B_off[13];
-          case 13: val += A_off[12] * B_off[12];
-          case 12: val += A_off[11] * B_off[11];
-          case 11: val += A_off[10] * B_off[10];
-          case 10: val += A_off[9] * B_off[9];
-          case 9: val += A_off[8] * B_off[8];
-          case 8: val += A_off[7] * B_off[7];
-          case 7: val += A_off[6] * B_off[6];
-          case 6: val += A_off[5] * B_off[5];
-          case 5: val += A_off[4] * B_off[4];
-          case 4: val += A_off[3] * B_off[3];
-          case 3: val += A_off[2] * B_off[2];
-          case 2: val += A_off[1] * B_off[1];
-          case 1: val += A_off[0] * B_off[0];
-            break;
-          default:
-            for (int yp = 0; yp < kernelDim; yp++) {
-              val += *(A_off++) * *(B_off++);
-            }
-        }
-      }
-      *(dst++) += val;
-    }
-  }
-  return true;
-}
-
 // -------------------------------------------------------
 // matlab entry point
 // outMap = fconv_c(inMap, kernel, inChannel, outChannel, table, shape)
@@ -303,3 +244,62 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
   }
 }
 
+
+bool convolve2D(double *in, int inDim, double *kernel, int kernelDim,
+                double *out) {
+// This function is from the following source:
+// AUTORIGHTS
+// -------------------------------------------------------
+// Copyright (C) 2011-2012 Ross Girshick
+// This file is part of the voc-releaseX code
+// (http://people.cs.uchicago.edu/~rbg/latent/)
+// and is available under the terms of an MIT-like license
+// provided in COPYING. Please retain this notice and
+// COPYING if you use this file (or a portion of it) in
+// your project.
+// -------------------------------------------------------
+  int outDim = inDim - kernelDim + 1;
+
+  double *A_src = in;
+  double *B_src = kernel;
+  double *dst = out;
+  // start convolution
+  for (int x = 0; x < outDim; x++) {
+    for (int y = 0; y < outDim; y++) {
+      double val = 0;
+      for (int xp = 0; xp < kernelDim; xp++) {
+        double *A_off = A_src + (x+xp)*inDim + y;
+        double *B_off = B_src + xp*kernelDim;
+        switch(kernelDim) {
+          case 20: val += A_off[19] * B_off[19];
+          case 19: val += A_off[18] * B_off[18];
+          case 18: val += A_off[17] * B_off[17];
+          case 17: val += A_off[16] * B_off[16];
+          case 16: val += A_off[15] * B_off[15];
+          case 15: val += A_off[14] * B_off[14];
+          case 14: val += A_off[13] * B_off[13];
+          case 13: val += A_off[12] * B_off[12];
+          case 12: val += A_off[11] * B_off[11];
+          case 11: val += A_off[10] * B_off[10];
+          case 10: val += A_off[9] * B_off[9];
+          case 9: val += A_off[8] * B_off[8];
+          case 8: val += A_off[7] * B_off[7];
+          case 7: val += A_off[6] * B_off[6];
+          case 6: val += A_off[5] * B_off[5];
+          case 5: val += A_off[4] * B_off[4];
+          case 4: val += A_off[3] * B_off[3];
+          case 3: val += A_off[2] * B_off[2];
+          case 2: val += A_off[1] * B_off[1];
+          case 1: val += A_off[0] * B_off[0];
+            break;
+          default:
+            for (int yp = 0; yp < kernelDim; yp++) {
+              val += *(A_off++) * *(B_off++);
+            }
+        }
+      }
+      *(dst++) += val;
+    }
+  }
+  return true;
+}
